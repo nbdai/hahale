ac5aa99af42c51ba1e02e903af60fb9dda685e85
dzl <daizhanglei@guazi.com>
2019-04-26 16:16:06 +0800

    Squashed commit of the following:
    commit c4aede4fbf5b5a3acd168cc5b518560e34a17634
    Author: dzl <daizhanglei@guazi.com>
    Date:   Fri Apr 26 16:14:25 2019 +0800
    
        fix
    
    commit a2883896bb5b6670b062b4ba3b9c67309cc27766
    Merge: 6774179 515fda6
    Author: dzl <daizhanglei@guazi.com>
    Date:   Fri Apr 26 16:02:03 2019 +0800
    
        Merge branch 'master' into mac
    
    commit 6774179ef63d65fceddd6dae489e0a7f3a6973c9
    Author: dzl <daizhanglei@guazi.com>
    Date:   Fri Apr 26 15:26:13 2019 +0800
    
        新增

--- back-service/src/main/java/com/dzl/backservice/VideoController.java
+++ back-service/src/main/java/com/dzl/backservice/VideoController.java
@@ -1,4 +1,17 @@
 package com.dzl.backservice;
+
+import com.dzl.pojo.Videos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class VideoController {
+    @Autowired
+    private  VideoService videoService;
+    @RequestMapping("saveVideo")
+    public String saveVideo(@RequestBody Videos videos){
+        return null;
+    }
 }
--- back-service/src/main/java/com/dzl/backservice/VideoService.java
+++ back-service/src/main/java/com/dzl/backservice/VideoService.java
@@ -1,4 +1,12 @@
 package com.dzl.backservice;
 
+import com.dzl.pojo.Videos;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VideoService {
+
+    public  String saveVideo(Videos videos){
+
+    }
 }
