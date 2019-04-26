6b77c06560f5154cc22625b323dbff9b3fa138f5
dzl <daizhanglei@guazi.com>
2019-04-26 14:56:49 +0800

    新增bgm 视频上传

---
+++ pojo/src/test/java/com/dzl/pojo/PojoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dzl.pojo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PojoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
