57eeace67241a6427ca3e116497e2ccfc9fd67d4
dzl <daizhanglei@guazi.com>
2019-04-26 15:53:50 +0800

    fix

--- protal/src/main/java/com/dzl/protal/VideoController.java
+++ protal/src/main/java/com/dzl/protal/VideoController.java
@@ -38,7 +38,7 @@ public class VideoController extends BasicController {
 	
     @Autowired
     private RestTemplate restTemplate;
-	//啊啊啊
+
 	@ApiOperation(value="上传视频", notes="上传视频的接口")
 	@ApiImplicitParams({
 		@ApiImplicitParam(name="userId", value="用户id", required=true, 
