b408d9fcaa31bdbce697a6900d9dae5264feb9be
dzl <daizhanglei@guazi.com>
2019-04-26 16:38:34 +0800

    fix

--- back-service/src/main/java/com/dzl/backservice/VideoController.java
+++ back-service/src/main/java/com/dzl/backservice/VideoController.java
@@ -12,10 +12,6 @@ public class VideoController {
     private  VideoService videoService;
     @RequestMapping("saveVideo")
     public String saveVideo(@RequestBody Videos videos){
-<<<<<<< HEAD
-        return null;
-=======
         return videoService.saveVide(videos);
->>>>>>> mac
     }
 }
