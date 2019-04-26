c1978640b4ceeee68cea53b3424c4020ce13b903
dzl <daizhanglei@guazi.com>
2019-04-26 14:41:25 +0800

    新增bgm 视频上传

---
+++ protal/src/main/java/com/dzl/protal/ProtalApplication.java
@@ -0,0 +1,20 @@
+package com.dzl.protal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
+@EntityScan(basePackages={"com.dzl.pojo"})
+public class ProtalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtalApplication.class, args);
+	}
+
+}
