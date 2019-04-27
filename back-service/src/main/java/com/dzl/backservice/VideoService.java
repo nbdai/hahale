1c456d851d6cb03d2cbf2ad09f733a00e794b538
DZL <1938542498@qq.com>
2019-04-27 19:28:39 +0800

    导包

--- back-service/src/main/java/com/dzl/backservice/VideoService.java
+++ back-service/src/main/java/com/dzl/backservice/VideoService.java
@@ -1,61 +1,23 @@
 package com.dzl.backservice;
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 import com.dzl.pojo.Videos;
-
 import com.dzl.dao.video.VideoReposity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class VideoService {
-
-    @Autowired
-    private VideoReposity videoReposity;
-    public String saveVide(Videos videos){
-       Videos video =  videoReposity.save(videos);
-        return video.getId();
-    }
-=======
-import com.dzl.dao.video.VideoReposity;
-import com.dzl.pojo.Videos;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
 @Service
 public class VideoService {
-=======
-import com.dzl.dao.video.VideoReposity;
-import com.dzl.pojo.Videos;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-public class VideoService {
->>>>>>> mac
     @Autowired
     private VideoReposity videoReposity;
     @Transactional(propagation = Propagation.REQUIRED)
-    public String saveVideo(Videos videos){
-       Videos video =  videoReposity.save(videos);
+    public String saveVideo(Videos videos) {
+        Videos video = videoReposity.save(videos);
         return video.getId();
     }
     @Transactional(propagation = Propagation.REQUIRED)
-   public Integer  updateVideo(String videoId, String uploadPathDB){
-     return videoReposity.updateCoverPathById(uploadPathDB,videoId);
-   }
-
-
-<<<<<<< HEAD
->>>>>>> mac
-=======
->>>>>>> mac
+    public Integer updateVideo(String videoId, String uploadPathDB) {
+        return videoReposity.updateCoverPathById(uploadPathDB, videoId);
+    }
 }
