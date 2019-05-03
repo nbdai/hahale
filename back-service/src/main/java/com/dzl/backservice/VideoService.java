a189b81132ac868a918cb800babb103b59635fdd
dzl <daizhanglei@guazi.com>
2019-05-03 10:12:58 +0800

    fix

--- back-service/src/main/java/com/dzl/backservice/VideoService.java
+++ back-service/src/main/java/com/dzl/backservice/VideoService.java
@@ -1,28 +1,17 @@
 package com.dzl.backservice;
 import com.dzl.pojo.Videos;
 import com.dzl.dao.video.VideoReposity;
-
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-=======
-import com.dzl.dao.video.VideoReposity;
-import com.dzl.pojo.Videos;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
->>>>>>> mac
 @Service
 public class VideoService {
     @Autowired
     private VideoReposity videoReposity;
     @Transactional(propagation = Propagation.REQUIRED)
-<<<<<<< HEAD
     public String saveVideo(Videos videos) {
         Videos video = videoReposity.save(videos);
         return video.getId();
@@ -31,7 +20,6 @@ public class VideoService {
     public Integer updateVideo(String videoId, String uploadPathDB) {
         return videoReposity.updateCoverPathById(uploadPathDB, videoId);
     }
-=======
     public String saveVideo(Videos videos){
        Videos video =  videoReposity.save(videos);
         return video.getId();
@@ -40,7 +28,4 @@ public class VideoService {
    public Integer  updateVideo(String videoId, String uploadPathDB){
      return videoReposity.updateCoverPathById(uploadPathDB,videoId);
    }
-
-
->>>>>>> mac
 }
